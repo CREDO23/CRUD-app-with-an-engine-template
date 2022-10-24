3f5b80a2985dec57b5826d8dc0e602b302f38077
Jospin Kanane <jospinkanane@yahoo.com>
2022-10-24 13:46:16 +0200

    Ajout d'un formulaire

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ const app = express();
 
 app.use(express.json());
 
+app.set("veiw", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-app.set("veiws", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => {
-  res.send("App is running");
+// app.get("/", (req, res) => {
+//   res.send("App is running");
+// });
+
+app.get("/", (req, res)=>{
+  res.render("home", {titre : "Welcome to CRUD-App!"})
 });
 
 app.listen(PORT, (error) => {
