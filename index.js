19d5e5b89a53516e12b30baabcdac3ee1f13956f
jethronbwira@gmail.com <jethronbwira@gmail.com>
2022-10-24 14:41:20 +0200

    feat: sequelize removed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const path = require("path");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
