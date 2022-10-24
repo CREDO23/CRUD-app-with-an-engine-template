9af37a217e6b1f6abb98a44f684c311dca96a7aa
CREDO23 <thierrybakera12@gmail.com>
2022-10-24 10:38:14 +0200

    init project

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("App is running");
+});
+
+app.listen(PORT, (error) => {
+  if (error) console.log(error);
+
+  console.log(`Server running on ${PORT}`);
+});
